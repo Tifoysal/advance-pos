c2cdf888a6021ff495cda1d31294229afb99400a
Tifoysal <foysal11203004@gmail.com>
2022-03-08 19:05:15 +0600

    update conflict into web

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\PermissionController;
-=======
->>>>>>> 681e38b7bee14cafce1a0ec35550da69b1ddc3aa
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
@@ -56,7 +53,6 @@ Route::post('update/role/{role_id}',[RoleController::class,'update'])->name('upd
 Route::get('view/role/{role_id}',[RoleController::class,'show'])->name('view.role');
 
 
-<<<<<<< HEAD
 // permission
 Route::get('/permissions',[PermissionController::class,'index'])->name('permission.index');
 Route::get('/permission/create',[PermissionController::class,'create'])->name('permission.create');
@@ -65,7 +61,6 @@ Route::post('/permission/store',[PermissionController::class,'store'])->name('pe
 //assign permissions
 Route::get('/permissions/assign/{role_id}',[RoleController::class,'assignForm'])->name('permission.assign.form');
 Route::post('/permissions/assign/store',[RoleController::class,'assignStore'])->name('permission.assign.store');
-=======
 
 Route::get('/role',[RolesController::class,'index'])->name('role.index');
 Route::get('/role/create',[RolesController::class,'create'])->name('role.create');
@@ -74,14 +69,3 @@ Route::get('/role_view/{role_id}', [RolesController::class, 'details_role'])->na
 Route::get('/role_edit/{role_id}',[RolesController::class, 'edit_role'])->name('role.edit');
 Route::post('/role_update/{role_id}',[RolesController::class, 'update_role'])->name('role.update');
 Route::get('/role_delete/{role_id}',[RolesController::class, 'delete_role'])->name('delete.role');
-// category
-Route::get('/category_list',[CategoryController::class,'list_category'])->name('category.list');
-Route::get('/category_add',[CategoryController::class,'add_category'])->name('category.add');
-Route::post('/category_store',[CategoryController::class,'store_category'])->name('category.store');
-Route::get('/category_view/{category_id}', [CategoryController::class, 'details_category'])->name('category.view');
-Route::get('/category_edit/{category_id}',[CategoryController::class, 'edit_category'])->name('category.edit');
-Route::post('/category_update/{category_id}',[CategoryController::class, 'update_category'])->name('category.update');
-Route::get('/category_delete/{category_id}', [CategoryController::class, 'delete_category'])->name('delete.category');
-
-
->>>>>>> 681e38b7bee14cafce1a0ec35550da69b1ddc3aa
